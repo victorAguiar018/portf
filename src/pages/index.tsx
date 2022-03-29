f3ab3e98ed2489b42bb76be51ea5e4f90c5b4a28
VictorAguiare <victoraguiar018@gmail.comm>
2022-03-29 12:23:45 -0300

    iniciando projeto de portifolio

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div>
+        <>
+          <Head>
+            <title>Victor Aguiar</title>
+            
+          </Head>
+          <h1>Hellow Word</h1>
+        </>
+    </div>
+  )
+}
