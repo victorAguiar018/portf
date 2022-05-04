874ae37df54c7d09db38026799e19dbac2c5f1e7
VictorAguiare <victoraguiar018@gmail.comm>
2022-05-03 22:54:51 -0300

    organizando codigo e adicioanndo nova branch

--- src/components/diferenciais/index.tsx
+++ src/components/diferenciais/index.tsx
@@ -7,7 +7,9 @@ export function Diferencias(){
             <div className={styles.diferencias}>
                 <h1>Diferenciais</h1>
             </div>
-            
+            <div className={styles.ContainerBarra}>
+                <div className={styles.barra}></div>
+            </div>
             <div className={styles.content}>
                 <div className={styles.contentItem}>
                     <div className={styles.item}>
